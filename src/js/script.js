1f33bc4b56b09346e5be04adce984761a751f8d9
liveevil1995 <shining1234@yandex.ru>
2021-05-20 21:09:01 +0300

    add slider

--- src/js/script.js
+++ src/js/script.js
@@ -139,4 +139,42 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
     window.addEventListener('scroll', showModalByScroll);
+
+    // slider
+
+    const slides = document.querySelectorAll('.offer__slide'),
+          prev = document.querySelector('.offer__slider-prev'),
+          next = document.querySelector('.offer__slider-next');
+    let slideIndex = 1;
+
+    showSlides(slideIndex);
+
+
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex(1);
+        }
+
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+        slides.forEach(item => item.style.display = 'none');
+
+        slides[slideIndex - 1].style.display = 'block';
+    }
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    prev.addEventListener('click', () => {
+        plusSlides(-1);
+    });
+
+    next.addEventListener('click', () => {
+        plusSlides(1);
+    });
 });
+
+
